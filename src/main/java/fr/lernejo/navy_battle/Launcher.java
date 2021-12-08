171e997778a07be2fcc693136d5c4d561c3f8850
Fabien Verspan <78422154+fab971cool@users.noreply.github.com>
2021-12-08 06:58:40 +0100

    add plugin for unit test

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,9 +1,28 @@
 package fr.lernejo.navy_battle;
 
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
 public class Launcher
 {
-    public static void main( String[] argc)
-    {
+    public static void main(String[] args) throws IOException {
 
+        if ( args.length != 1)
+        {
+            System.out.println("Veuillez spécifier le port d'écoute");
+            System.exit(1);
+        }
+
+        Server server = new Server(9876);
+        server.run();
     }
 }
