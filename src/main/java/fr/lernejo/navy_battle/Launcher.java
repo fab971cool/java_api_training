20e8ca3c3fef43d1248f4bbbbcf68e9c41522325
Fabien Verspan <78422154+fab971cool@users.noreply.github.com>
2021-12-23 13:00:07 +0100

    Correction for Bot

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -14,27 +14,17 @@ public class Launcher
         if ( args.length < 1 || args.length > 2){
             System.out.println("Veuillez spécifier le port d'écoute");
             System.exit(1);
-        }int number = 0;
+        }
         try {
+            int number;
             number = Integer.parseInt(args[0]);
             Server server = new Server();
             server.run(number);
-            if (args.length == 2) { new Launcher().requestServer(args[1], args[0]);}
+            if (args.length == 2) { server.requestServer(args[1], args[0]);}
         }
         catch (Exception e) {
             System.out.println("Not an Integer");
             throw new NumberFormatException(e.getMessage());
         }
-    }
-    public void requestServer( String serverUrl, String portNumber) throws IOException, InterruptedException {
-        var client = HttpClient.newHttpClient();
-        HttpRequest requestePost = HttpRequest.newBuilder()
-            .uri(URI.create(serverUrl + "/api/game/start"))
-            .setHeader("Accept", "application/json")
-            .setHeader("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"1\", \"url\":\"http://localhost:" + portNumber + "\", \"message\":\"hello\"}"))
-            .build();
-        var response = client.send(requestePost, HttpResponse.BodyHandlers.ofString());
-        System.out.println("Connection OK. \nReceived:\t"+response.body() +"\nStatus :\t" + response.statusCode());
     }
 }
--- src/main/java/fr/lernejo/navy_battle/Server.java
+++ src/main/java/fr/lernejo/navy_battle/Server.java
@@ -6,6 +6,10 @@ import fr.lernejo.navy_battle.ServerHandler.CallHandler;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.concurrent.Executors;
 
 public class Server
@@ -26,5 +30,15 @@ public class Server
 
         server.start();
     }
-
+    public void requestServer( String serverUrl, String portNumber) throws IOException, InterruptedException {
+        var client = HttpClient.newHttpClient();
+        HttpRequest requestePost = HttpRequest.newBuilder()
+            .uri(URI.create(serverUrl + "/api/game/start"))
+            .setHeader("Accept", "application/json")
+            .setHeader("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"1\", \"url\":\"http://localhost:" + portNumber + "\", \"message\":\"hello\"}"))
+            .build();
+        var response = client.send(requestePost, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Connection OK. \nReceived:\t"+response.body() +"\nStatus :\t" + response.statusCode());
+    }
 }
